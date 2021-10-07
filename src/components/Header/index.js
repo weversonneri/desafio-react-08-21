c9bb5bcb36ba109ca3e94a9165ca872d7cb99246
F0716627 Amon Gabriel dos Santos Paiva <amon_gabriel@bb.com.br>
2021-10-07 15:40:17 -0300

    alteração de ícones

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,14 @@
-import bb from '../../assets/bb.jpeg';
+import react from '../../assets/react.jpeg';
 import './styles.css';
 
 function Header() {
   return (
-    <nav className="navbar navbar-bb fixed-top flex-md-nowrap bg-bb">
+    <nav className="navbar navbar-react fixed-top flex-md-nowrap bg-react">
       <div className="navbar-item-1">
-        <img src={bb} className="logo-bb" alt="bb" />
+        <img src={react} className="logo-react" alt="react" />
       </div>
       <div className="navbar-item-2">
-        <h3>Desafio BB - Exposição de dados de servidores em Datatable</h3>
+        <h3>Desafio React JS - Exposição de dados de servidores em Datatable</h3>
       </div>
     </nav>
   );
